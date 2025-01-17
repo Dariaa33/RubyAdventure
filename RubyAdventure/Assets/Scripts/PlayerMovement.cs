87e581fa02153597f1ddd70eec00338f7e409802
Dariaa33 <vesnayoon@gmail.com>
2025-01-17 10:48:28 +0100

    17.01

--- RubyAdventure/Assets/Scripts/PlayerMovement.cs
+++ RubyAdventure/Assets/Scripts/PlayerMovement.cs
@@ -6,51 +6,27 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public InputAction LeftAction;
     public InputAction MoveAction;
+    Rigidbody2D rb2D;
+    Vector2 move;
+    [SerializeField]
     float speed = 0.1f;
 
-    /*float horizontal = 0.0f;
-    float vertical = 0.0f;
-    [SerializeField]
-    */
     void Start()
     {
         MoveAction.Enable();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * 0.1f * speed;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+     
+    }
 
-        /*if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            horizontal = -1.0f;
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            horizontal = 1.0f;
-        }
-      
-
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            vertical = -1.0f;
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            vertical = 1.0f;
-        }
-       
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal * speed;
-        position.y = position.y + 0.1f * vertical * speed;
-        transform.position = position;*/
-
-
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rb2D.position + move * 3.0f * Time.deltaTime * speed;
+        rb2D.MovePosition(position);
     }
 }
